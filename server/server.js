1fa0a4a2a2d94e9963fb5b8e72f413a25343a09f
rohan <rohanverma031@gmail.com>
2024-03-17 22:11:52 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -7,7 +7,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT ?? 8080;
 
-app.use(express.static("dist"));
+if (process.env.ENV === "production") {
+	app.use(express.static("dist"));
+} else {
+	app.use(express.static("../client/dist"));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- server/src/controllers/home.js
+++ server/src/controllers/home.js
@@ -1,15 +1,33 @@
 const path = require("path");
+const dotenv = require("dotenv");
 
-const homeHtml = path.join(__dirname, "/../../client/dist/index.html");
-const portfolioHTMl = path.join(__dirname, "/../../client/dist/portfolio.html");
-const notfoundHTML = path.join(__dirname, "/../../client/dist/404.html");
+// configuring for env variables
+dotenv.config();
+
+// const homeHtml = path.join(__dirname, "/../../client/dist/index.html");
+// const portfolioHTMl = path.join(__dirname, "/../../client/dist/portfolio.html");
+// const notfoundHTML = path.join(__dirname, "/../../client/dist/404.html");
+
+let homeHTML;
+let portfolioHTML;
+let notfoundHTML;
+
+if (process.env.ENV === "production") {
+	homeHTML = path.join(__dirname, "../../dist/index.html");
+	portfolioHTML = path.join(__dirname, "../../dist/portfolio.html");
+	notfoundHTML = path.join(__dirname, "../../dist/404.html");
+} else {
+	homeHTML = path.join(__dirname, "../../../client/dist/index.html");
+	portfolioHTML = path.join(__dirname, "../../../client/dist/portfolio.html");
+	notfoundHTML = path.join(__dirname, "../../../client/dist/404.html");
+}
 
 const home = (req, res) => {
-	return res.sendFile(homeHtml);
+	return res.sendFile(homeHTML);
 };
 
 const portfolio = (req, res) => {
-	return res.sendFile(portfolioHTMl);
+	return res.sendFile(portfolioHTML);
 };
 
 const contactus = (req, res) => {

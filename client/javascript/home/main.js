168c7cd4e3635233b3daa19331e7a580a380903a
rohan <rohanverma031@gmail.com>
2024-03-27 19:54:43 +0530

    portfolio

--- client/javascript/home/main.js
+++ client/javascript/home/main.js
@@ -4,7 +4,8 @@ import { initCanvas } from "../module/canvas/canvas";
 window.addEventListener("load", () => {
 	// get the main element
 	const main = document.getElementsByTagName("main")[0];
+	const portfolio = document.querySelector(".portfolio-container");
 
 	initCanvas();
-	scrollAnimation(main);
+	scrollAnimation(main, portfolio);
 });
--- client/javascript/module/scroll.js
+++ client/javascript/module/scroll.js
@@ -6,6 +6,7 @@ let height = window.innerHeight;
 
 export const scrollAnimation = (...domElements) => {
 	const main = domElements[0];
+	const portfolio = domElements[1];
 
 	main.style.setProperty("--times", heroTimes);
 
@@ -17,11 +18,21 @@ export const scrollAnimation = (...domElements) => {
 	window.addEventListener("scroll", (e) => {
 		let y = scrollY;
 
-		// hero section height * heroTimes;
 		const heroHeight = height * heroTimes;
+		const portfolioStart = Math.round(
+			portfolio.getBoundingClientRect().top +
+				window.scrollY -
+				height / 1.2
+		);
 
 		if (y <= heroHeight) {
 			handleHeroSection(y, heroHeight, main);
+		}
+
+		if (y >= portfolioStart) {
+			let offsetY = y - portfolioStart;
+
+			handlePortfolioSection(offsetY, portfolio);
 		}
 	});
 };
@@ -40,5 +51,37 @@ const handleHeroSection = (y, height, main) => {
 	opacity = Math.round(opacity * 10) / 10;
 
 	main.style.setProperty("--opacity", opacity);
-	console.log(opacity);
+};
+
+const handlePortfolioSection = (y, portfolio) => {
+	const heading = portfolio.childNodes[1].childNodes[1];
+	// const headingHeight = heading.offsetHeight;
+	const headingFill = height / 2;
+
+	const items = portfolio.childNodes[1].childNodes[3].childNodes;
+
+	if (y <= headingFill) {
+		let backgroundSize = (y / headingFill) * 100;
+
+		portfolio.style.setProperty(
+			"--background-size",
+			backgroundSize + 1 + "%"
+		);
+	}
+
+	for (let i = 1; i < items.length; i += 2) {
+		// if (i != 1) continue;
+		let item = items[i];
+		let top = Math.round(item.getBoundingClientRect().top - height * 1.1);
+		// let bottom = item.getBoundingClientRect().bottom - height;
+
+		if (top <= 0) {
+			let scale = Math.max((y / height / 0.7) * 1, 0.5);
+			scale = Math.min(1, scale);
+			let opacity = scale;
+
+			item.style.setProperty("--scale", scale);
+			item.style.setProperty("--opacity", opacity);
+		}
+	}
 };

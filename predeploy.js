5730d26b6d01bb851e1b2f0f4a6621cc23796e10
rohan <rohanverma031@gmail.com>
2024-04-26 00:14:14 +0530

    updated predeploy script

--- predeploy.js
+++ predeploy.js
@@ -4,11 +4,27 @@ const { resolve } = require("path");
 const sourcePath = resolve(__dirname, "client/dist");
 const destinationPath = resolve(__dirname, "server/dist");
 
+// remove folder
+const dir = "./client";
+
+const removeClient = () => {
+	fs.rm(dir, { recursive: true, force: true }, (err) => {
+		if (err) {
+			throw err;
+		}
+
+		console.log(`${dir} is deleted!`);
+	});
+};
+
 // Move folder
-fs.move(sourcePath, destinationPath, (err) => {
+fs.move(sourcePath, destinationPath, { overwrite: true }, (err) => {
 	if (err) {
 		console.error("Error moving folder:", err);
 	} else {
 		console.log("Folder moved successfully");
+
+		if (process.env.ENV === "production") removeClient();
+		else console.log("skip deleting client files");
 	}
 });
